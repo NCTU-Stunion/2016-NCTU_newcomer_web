c53a2185e7146f7669d86dce043d221118a3ea73
Kamigami55 <samkami@icloud.com>
2016-08-06 16:55:26 +0800

    Move club data to seeds.rb

--- ror_version/app/models/newcomer_club.rb
+++ ror_version/app/models/newcomer_club.rb
@@ -2,13 +2,12 @@ class NewcomerClub < ActiveRecord::Base
     # must contain :category & :name
     validates_presence_of :category, :name
 
-    # validate :pdf, :img, :fb, :web value
-    validates_format_of :pdf, :with => /.+\.pdf/, :allow_empty => true
-    validates_format_of :img, :allow_empty => true
-    validates_format_of :fb, :with => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix, :allow_empty => true
-    validates_format_of :web, :with => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix, :allow_empty => true
+    # validate :pdf, :fb, :web value
+    validates_format_of :pdf, :with => /.+\.pdf/, :allow_nil => true
+    validates_format_of :fb, :with => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix, :allow_nil => true
+    validates_format_of :web, :with => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix, :allow_nil => true
 
     # validate :category, :color value
     validates_inclusion_of :category, :in => ["curricular", "studentAssociation", "alumnian"]
-    validates_inclusion_of :color, :in => ["white", "milk", "grey"], :allow_empty => true
+    validates_inclusion_of :color, :in => ["white", "milk", "grey", "default"], :allow_nil => true
 end
